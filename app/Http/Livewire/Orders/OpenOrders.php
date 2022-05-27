41071e9a2ecf8f64de7d696298dc84d5c5f77593
raul <neculai.inocentiu@gmail.com>
2022-05-27 11:16:17 +0300

    sidebar

---
+++ app/Http/Livewire/Orders/OpenOrders.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire\Orders;
+
+use Livewire\Component;
+
+class OpenOrders extends Component
+{
+    public function render()
+    {
+        return view('livewire.orders.open-orders')->extends('layouts.app')
+            ->section('content');
+    }
+}
