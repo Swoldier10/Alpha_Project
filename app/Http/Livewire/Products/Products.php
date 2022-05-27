02d3399917657f319e719ac4b587413f51b126c9
raul <neculai.inocentiu@gmail.com>
2022-05-27 11:59:33 +0300

    sidebar

--- app/Http/Livewire/Products/Products.php
+++ app/Http/Livewire/Products/Products.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Livewire\Products;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class Products extends Component
 {
+    public $products = [];
     public function render()
     {
         return view('livewire.products.products')->extends('layouts.app')
             ->section('content');
+    }
+
+    public function mount(){
+        $this->products = Product::all();
     }
 }
